004c906bef1b915d31dbf52a09af169db5571bb3
Waldir <waldirjunior.gpn@gmail.com>
2023-08-04 06:10:14 -0300

    Validação do horário de funcionamento

--- src/main/java/med/voll/api/domain/consulta/validacao/ValidarHorarioFuncionamento.java
+++ src/main/java/med/voll/api/domain/consulta/validacao/ValidarHorarioFuncionamento.java
@@ -1,11 +1,22 @@
 package med.voll.api.domain.consulta.validacao;
 
+import med.voll.api.domain.ValidacaoException;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
 
-public class ValidarHorarioFuncionamento implements validacao{
+import java.time.DayOfWeek;
+
+public class ValidarHorarioFuncionamento implements validacao {
 
     @Override
     public void validar(DadosAgendamentoConsulta dados) {
+        var dataConsulta = dados.data();
 
+        var domingo = dataConsulta.getDayOfWeek().equals(DayOfWeek.SUNDAY);
+        var antesDaAberturaDaClinica = dataConsulta.getHour() < 7;
+        var depoisDoEncerramentoDaClinica = dataConsulta.getHour() > 18;
+
+        if (domingo || antesDaAberturaDaClinica || depoisDoEncerramentoDaClinica) {
+            throw new ValidacaoException("Consulta fora do horário de funcionamento da Clínica");
+        }
     }
 }
