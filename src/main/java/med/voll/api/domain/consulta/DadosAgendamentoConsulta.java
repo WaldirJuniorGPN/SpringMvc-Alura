1e973511a3de9fb739044fcd11a6c9e63febf8b8
Waldir <waldirjunior.gpn@gmail.com>
2023-08-06 18:35:22 -0300

    Correção da dto DadosAgendamentoConsulta

--- src/main/java/med/voll/api/domain/consulta/DadosAgendamentoConsulta.java
+++ src/main/java/med/voll/api/domain/consulta/DadosAgendamentoConsulta.java
@@ -9,7 +9,6 @@ import java.time.LocalDateTime;
 public record DadosAgendamentoConsulta(
         @NotNull
         Long idPaciente,
-        @NotNull
         Long idMedico,
         @NotNull
         @Future
